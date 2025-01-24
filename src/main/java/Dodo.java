39d4ba82e1be95034a86222ba16cadd961d34fec
potatodudedude <szyptd2002@gmail.com>
2025-01-24 13:08:26 +0800

    Added copycat and bye function

--- src/main/java/Dodo.java
+++ src/main/java/Dodo.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Dodo {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
         String logo = " _____   ____  _____   ____  \n"
                 + "|  __ \\ / __ \\|  __ \\ / __ \\ \n"
                 + "| |  | | |  | | |  | | |  | |\n"
@@ -7,6 +10,13 @@ public class Dodo {
                 + "| |__| | |__| | |__| | |__| |\n"
                 + "|_____/ \\____/|_____/ \\____/ \n";
         System.out.println(logo);
-        System.out.println("Hello! This is Dodo.\n" + "Ok bye!\n");
+        System.out.println("Hello! This is Dodo.\n" + "What is your command?\n");
+        while (true) {
+            String nextLine = userInput.nextLine();
+            if (nextLine.equals("bye")) {
+                System.out.println("See You! :)\n");
+                break;}
+            System.out.println(nextLine);
+        }
     }
 }

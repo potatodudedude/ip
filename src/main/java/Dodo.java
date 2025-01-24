506864a73d9d4f62eaded08d03330ad0fee0edec
potatodudedude <szyptd2002@gmail.com>
2025-01-24 18:49:02 +0800

    Added exceptions to handle array out of bounds after splitting command strings Added help function Added exceptions for invalid commands

--- src/main/java/Dodo.java
+++ src/main/java/Dodo.java
@@ -2,10 +2,50 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Dodo {
+    private static ArrayList<Task> tasks = new ArrayList<Task>();
+    public static void markCommandCheck(String[] commands) throws DodoException {
+        if (commands.length != 2)  {
+            throw new DodoException("Mark/Unmark commands needs to be followed by single task number.\n" +
+                    "e.g. mark 2");
+        }
+    }
+
+    public static void deadlineCommandCheck(String[] commands) throws DodoException {
+        if (commands.length != 2)  {
+            throw new DodoException("Deadline commands needs to be structured as follows:\n" +
+                    "deadline 'name' /by 'time'");
+        }
+    }
+
+    public static void eventCommandCheck(String[] commands) throws DodoException {
+        if (commands.length != 3)  {
+            throw new DodoException("Event commands needs to be structured as follows:\n" +
+                    "event 'name' /from 'start time' /to 'end time'");
+        }
+    }
+
+    public static void validTaskNumber(int i) throws DodoException {
+        if (i > tasks.size() || i < 1) {
+            throw new DodoException("Task number " + i + " doesn't exist dodohead!");
+        }
+    }
+
+    public static void redundantMark(int targetNo, boolean done) throws DodoException {
+        Task target = tasks.get(targetNo - 1);
+        if (done) {
+            if (target.getMark()) {
+                throw new DodoException("Following task is already marked done:\n" + target.toString());
+            }
+        } else {
+            if (!target.getMark()) {
+                throw new DodoException("Following task is already marked undone:\n" + target.toString());
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        int dodoheadCount = 0;
         Scanner userInput = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<Task>();
-        String regex = "\\s";
         String logo = " _____   ____  _____   ____  \n"
                 + "|  __ \\ / __ \\|  __ \\ / __ \\ \n"
                 + "| |  | | |  | | |  | | |  | |\n"
@@ -13,30 +53,43 @@ public class Dodo {
                 + "| |__| | |__| | |__| | |__| |\n"
                 + "|_____/ \\____/|_____/ \\____/ \n";
         System.out.println(logo);
-        System.out.println("Hello! This is Dodo.\n" + "What is your command?");
+        System.out.println("Dododo do dododo do dododo.\n" + "What is your command?");
         while (true) {
-            String nextLine = userInput.nextLine();
-            if (nextLine.equals("bye") || nextLine.equals("Bye")) {
-                System.out.println("See You! :)");
-                break;
+            if (dodoheadCount > 3) {
+                dodoheadCount = 0;
+                System.out.println("You seem to be struggling. Type 'help' if you need to see the command list.");
             }
-            if (nextLine.isEmpty()) {
+            String nextLine = userInput.nextLine();
+            if (nextLine.equals("bye") || nextLine.equals("BB")) {
+                System.out.println("K bye bye :)");
+                break;
+            } else if (nextLine.equals("list")) {
+                if (tasks.isEmpty()) {
+                    System.out.println("You have no tasks. Yay!");
+                    continue;
+                }
+                System.out.println("Here are your tasks:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    int taskNo = i + 1;
+                    System.out.println(taskNo + ". " + tasks.get(i).toString());
+                }
+                continue;
+            } else if (nextLine.isEmpty()) {
+                dodoheadCount++;
                 System.out.println("You've gotta enter a command first dodo head!");
                 continue;
+            } else if (nextLine.equals("help")) {
+                report();
+                continue;
             }
-            String[] nextLineArr = nextLine.split(regex, 2);
+
+            String[] nextLineArr = nextLine.split("\\s", 2);
+            if (nextLineArr.length == 1) {
+                dodoheadCount++;
+                System.out.println("Huh?");
+                continue;
+            }
             switch (nextLineArr[0]) {
-                case "list":
-                    if (tasks.isEmpty()) {
-                        System.out.println("You have no tasks. Yay!");
-                        break;
-                    }
-                    System.out.println("Here are your tasks:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        int taskNo = i + 1;
-                        System.out.println(taskNo + ". " + tasks.get(i).toString());
-                    }
-                    break;
                 case "todo": {
                     Task newTask = new Todo(nextLineArr[1]);
                     tasks.add(newTask);
@@ -44,69 +97,86 @@ public class Dodo {
                     break;
                 }
                 case "deadline": {
-                    String[] details = nextLineArr[1].split(" /by ",2);
+                    String[] details = nextLineArr[1].split(" /by ", 2);
+                    try {
+                        deadlineCommandCheck(details);
+                    } catch (DodoException ex) {
+                        System.out.println(ex.getMessage());
+                        break;
+                    }
                     Task newTask = new Deadline(details[0], details[1]);
                     tasks.add(newTask);
                     System.out.println("Added this to your list:\n" + newTask.toString());
                     break;
                 }
                 case "event": {
-                    String[] details = nextLineArr[1].split(" \\/from | \\/to ",3);
+                    String[] details = nextLineArr[1].split(" \\/from | \\/to ", 3);
+                    try {
+                        eventCommandCheck(details);
+                    } catch (DodoException ex) {
+                        System.out.println(ex.getMessage());
+                        break;
+                    }
                     Task newTask = new Event(details[0], details[1], details[2]);
                     tasks.add(newTask);
                     System.out.println("Added this to your list:\n" + newTask.toString());
                     break;
                 }
-                case "mark":
-                    try{
+                case "mark": {
+                    try {
+                        markCommandCheck(nextLineArr);
                         int targetNo = Integer.parseInt(nextLineArr[1]);
-                    }
-                    catch (NumberFormatException ex){
-                        System.out.println("Mark command needs to be done as follows without brackets:\n" +
-                                "mark (task number)");
+                        validTaskNumber(targetNo);
+                        redundantMark(targetNo, true);
+                    } catch (DodoException ex) {
+                        System.out.println(ex.getMessage());
+                        break;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("Task number given was not a number");
                         break;
                     }
-                    int doneTarget = Integer.parseInt(nextLineArr[1]);
-                    if (doneTarget > tasks.size() || doneTarget < 1) {
-                        System.out.println("Task number " + doneTarget + " doesn't exist dodohead!");
-                        break;
-                    }
-                    doneTarget--;
-                    Task doneTask = tasks.get(doneTarget);
-                    if (doneTask.getMark()) {
-                        System.out.println("Following task is already marked done:\n" + doneTask.toString());
-                        break;
-                    }
-                    doneTask.markDone();
-                    System.out.println("Marked as done:\n" + doneTask.toString());
+                    int targetNo = Integer.parseInt(nextLineArr[1]) - 1;
+                    Task target = tasks.get(targetNo);
+                    target.markDone();
+                    System.out.println("Marked as done:\n" + target.toString());
                     break;
-                case "unmark":
-                    try{
+                }
+                case "unmark":{
+                    try {
+                        markCommandCheck(nextLineArr);
                         int targetNo = Integer.parseInt(nextLineArr[1]);
-                    }
-                    catch (NumberFormatException ex){
-                        System.out.println("Unmark command needs to be done as follows without brackets:\n" +
-                                "unmark (task number)");
+                        validTaskNumber(targetNo);
+                        redundantMark(targetNo, false);
+                    } catch (DodoException ex) {
+                        System.out.println(ex.getMessage());
+                        break;
+                    } catch (NumberFormatException ex) {
+                        System.out.println("Task number given was not a number");
                         break;
                     }
-                    int undoneTarget = Integer.parseInt(nextLineArr[1]);
-                    if (undoneTarget > tasks.size() || undoneTarget < 1) {
-                        System.out.println("Task number " + undoneTarget + " doesn't exist dodohead!");
-                        break;
-                    }
-                    undoneTarget--;
-                    Task undoneTask = tasks.get(undoneTarget);
-                    if (!undoneTask.getMark()) {
-                        System.out.println("Following task is already marked undone:\n" + undoneTask.toString());
-                        break;
-                    }
-                    undoneTask.markUndone();
-                    System.out.println("Marked as undone:\n" + undoneTask.toString());
+                    int targetNo = Integer.parseInt(nextLineArr[1]) - 1;
+                    Task target = tasks.get(targetNo);
+                    target.markUndone();
+                    System.out.println("Marked as undone:\n" + target.toString());
                     break;
+                }
                 default:
-                    System.out.println("Sorry, I don't recognise this command :(");
+                    System.out.println("Huh?");
+                    dodoheadCount++;
                     break;
             }
         }
     }
+
+    private static void report() {
+        System.out.println("You got it boss! Here you go:\n" +
+                "list -> lists all current tasks and their numbering\n" +
+                "todo 'name' -> adds a task called 'name'\n" +
+                "deadline 'name' /by 'time' -> adds a task called 'name' with deadline of 'time'\n" +
+                "event 'name' /from 'start' to 'end' -> adds a task called 'name' with timeframe from 'start' to 'end'\n" +
+                "mark 'task number' -> marks corresponding task as done\n" +
+                "unmark 'task number' -> marks corresponding task as undone\n" +
+                "bye -> dododo");
+    }
+
 }
---
+++ src/main/java/DodoException.java
@@ -0,0 +1,9 @@
+public class DodoException extends Exception{
+    public DodoException() {
+        super();
+    }
+
+    public DodoException(String message) {
+        super(message);
+    }
+}

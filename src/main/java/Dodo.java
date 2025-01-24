a2c90a5986424b2beda8c690315d4920df70b095
potatodudedude <szyptd2002@gmail.com>
2025-01-24 12:53:57 +0800

    Removed Duke Added Dodo

---
+++ src/main/java/Dodo.java
@@ -0,0 +1,12 @@
+public class Dodo {
+    public static void main(String[] args) {
+        String logo = " _____   ____  _____   ____  \n"
+                + "|  __ \\ / __ \\|  __ \\ / __ \\ \n"
+                + "| |  | | |  | | |  | | |  | |\n"
+                + "| |  | | |  | | |  | | |  | |\n"
+                + "| |__| | |__| | |__| | |__| |\n"
+                + "|_____/ \\____/|_____/ \\____/ \n";
+        System.out.println(logo);
+        System.out.println("Hello! This is Dodo.\n" + "Ok bye!\n");
+    }
+}
